d2dbc7bfdc549d7c9aa89eeaf44be9bb9b232794
Esen Zheenchoroev <stefunices@gmail.com>
2020-07-28 15:06:19 +0300

    feat: set up webpack for react

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,82 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {};
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+  return config;
+};
+
+const babelOptions = (preset) => {
+  const opts = {
+    presets: ["@babel/preset-env"],
+  };
+  if (preset) {
+    opts.presets.push(preset);
+  }
+  return opts;
+};
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: "development",
-    entry:["./index.js"],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js",
-        publicPath: "/"
-    },
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: ["@babel/polyfill", "./index.jsx"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  devServer: {
+    port: 4200,
+    hot: isDev,
+  },
+  optimization: optimization(),
+  devtool: isDev ? "source-map" : "",
+  resolve: {
+    extensions: [".js", ".jsx", ".css"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: babelOptions(),
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: babelOptions("@babel/preset-react"),
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      minify: { collapseWhitespace: isProd },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
 };
